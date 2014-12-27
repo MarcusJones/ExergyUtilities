905a92043f86619a7582cfea967736ca4c10f391
MarcusJones <jones.0bj3@gmail.com>
2014-12-27 17:26:06 +0100

    Update

---
+++ ExergyUtilities/utility_logger.py
@@ -0,0 +1,44 @@
+#===============================================================================
+# Title of this Module
+# Authors; MJones, Other
+# 00 - 2012FEB05 - First commit
+# 01 - 2012MAR17 - Update to ...
+#===============================================================================
+
+"""This module does A and B. 
+Etc.
+"""
+
+#===============================================================================
+# Set up
+#===============================================================================
+# Standard:
+from __future__ import division    
+
+from config import *
+
+import logging.config
+import unittest
+
+LOGGING_CONFIG_1 = '%(funcName)-20s %(levelno)-3s: %(message)s'
+LOGGING_CONFIG_2 = '%(module)-20s %(funcName)-20s %(levelno)-3s: %(message)s'
+
+#===============================================================================
+# Code
+#===============================================================================
+class LoggerCritical:
+    def __enter__(self):
+        myLogger = logging.getLogger()
+        myLogger.setLevel("CRITICAL")
+    def __exit__(self, type, value, traceback):
+        myLogger = logging.getLogger()
+        myLogger.setLevel("DEBUG")
+        
+class LoggerDebug:
+    def __enter__(self):
+        myLogger = logging.getLogger()
+        myLogger.setLevel("DEBUG")
+    def __exit__(self, type, value, traceback):
+        myLogger = logging.getLogger()
+        myLogger.setLevel("DEBUG")
+

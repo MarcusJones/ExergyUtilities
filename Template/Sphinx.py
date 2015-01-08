dd79040416debacc79a20d6e57a5094c512cd218
MarcusJones <jones.0bj3@gmail.com>
2015-01-08 14:28:17 +0100

    Added sphinx notes

---
+++ Template/Sphinx.py
@@ -0,0 +1,23 @@
+'''
+Created on 08.01.2015
+
+@author: jon
+'''
+
+"""
+Function 
+.. py:function::
+
+
+
+Class
+.. py:class:: name(parameters)
+ 
+Attribute
+.. py:attribute:: name
+
+Method
+.. py:method::
+.. py:classmethod::
+
+"""

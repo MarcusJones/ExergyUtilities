e7bbf5ec68847c435da6789e5735ffd84b2a281c
MarcusJones <jones.0bj3@gmail.com>
2017-05-09 10:37:45 +0200

    Config and template updated

--- Template/Template testing.py
+++ Template/Template testing.py
@@ -6,6 +6,31 @@
 #===============================================================================
 # Standard:
 
+from config import *
+
+import logging.config
+import unittest
+
+
+from utility_inspect import get_self
+from utilities_xml import get_table_all_names, print_table
+from idf_parser import IDF
+import os.path as path
+
+#===============================================================================
+# Logging
+#===============================================================================
+print(ABSOLUTE_LOGGING_PATH)
+logging.config.fileConfig(ABSOLUTE_LOGGING_PATH)
+myLogger = logging.getLogger()
+myLogger.setLevel("DEBUG")
+
+
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    this_path = path.dirname(path.dirname(path.abspath(__file__)))
+    sys.path.append(this_path)
+    logging.debug("ADDED TO PATH: ".format(this_path))
 
 import unittest
 
@@ -14,6 +39,13 @@ import logging
 logging.basicConfig(format='%(funcName)-20s %(levelno)-3s: %(message)s', level=logging.DEBUG, datefmt='%I:%M:%S')
 my_logger = logging.getLogger()
 my_logger.setLevel("DEBUG")
+
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    this_path = path.dirname(path.dirname(path.abspath(__file__)))
+    sys.path.append(this_path)
+    logging.debug("ADDED TO PATH: ".format(this_path))
+
 
 
 # External 

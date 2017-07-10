37e4d607a78e046ac1ea3d7de41cc7af76b4a610
MarcusJones <jones.0bj3@gmail.com>
2017-07-10 16:28:09 +0200

    NMC

--- config/config_ces_laptop.py
+++ config/config_ces_laptop.py
@@ -1,1 +1,3 @@
 ABSOLUTE_LOGGING_PATH = r"C:\LOCAL_REPO\py_ExergyUtilities\LoggingConfig\loggingNofile.conf"
+IDF_TEMPLATE_PATH = r"C:\LOCAL_REPO\ref_energyDB\IDF_Library"
+FREELANCE_DIR = r"C:\Dropbox\16336 LIDL\\"

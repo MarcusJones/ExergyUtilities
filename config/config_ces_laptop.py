90ecd442c6b0ed9f5d63faa0ee90d3e43cc7c375
MarcusJones <jones.0bj3@gmail.com>
2017-07-14 15:22:25 +0200

    Added IDD xml path

--- config/config_ces_laptop.py
+++ config/config_ces_laptop.py
@@ -2,3 +2,4 @@ ABSOLUTE_LOGGING_PATH = r"C:\LOCAL_REPO\py_ExergyUtilities\LoggingConfig\logging
 IDF_TEMPLATE_PATH = r"C:\LOCAL_REPO\ref_energyDB\IDF_Templates"
 FREELANCE_DIR = r"C:\Dropbox\16336 LIDL\\"
 IDD_FILE_PATH = r"C:\LOCAL_REPO\py_LEED_IDF_ESO\IDD\EnergyPlus 8 6 0.idd"
+IDD_XML_FILE_PATH = r"C:\LOCAL_REPO\py_EnergyParser\IDD_XML\IDD_860.xml"

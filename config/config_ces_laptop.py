721a6ab906f18771f47cf18a11c427d9876e87af
MarcusJones <jones.0bj3@gmail.com>
2017-09-27 15:21:02 +0200

    NMC

--- config/config_ces_laptop.py
+++ config/config_ces_laptop.py
@@ -1,5 +1,4 @@
 ABSOLUTE_LOGGING_PATH = r"C:\LOCAL_REPO\py_ExergyUtilities\LoggingConfig\loggingNofile.conf"
-
 IDD_FILE_PATH = r"C:\LOCAL_REPO\py_LEED_IDF_ESO\IDD\EnergyPlus 8 6 0.idd"
 IDD_XML_FILE_PATH = r"C:\LOCAL_REPO\py_EnergyParser\IDD_XML\IDD_860.xml"
 POST_PROC_EXCEL_FILE = r"C:\LOCAL_REPO\py_LEED_IDF_ESO\ExcelTemplates\LEED PostProcess r33.xlsx"

ec32aca18186a070993cfa802455de5a4de05e74
MarcusJones <jones.0bj3@gmail.com>
2018-01-08 18:03:50 +0100

    Minor changes

--- config/config_ces_laptop.py
+++ config/config_ces_laptop.py
@@ -1,4 +1,1 @@
 ABSOLUTE_LOGGING_PATH = r"C:\LOCAL_REPO\py_ExergyUtilities\LoggingConfig\loggingNofile.conf"
-IDD_FILE_PATH = r"C:\LOCAL_REPO\py_LEED_IDF_ESO\IDD\EnergyPlus 8 6 0.idd"
-IDD_XML_FILE_PATH = r"C:\LOCAL_REPO\py_EnergyParser\IDD_XML\IDD_860.xml"
-POST_PROC_EXCEL_FILE = r"C:\LOCAL_REPO\py_LEED_IDF_ESO\ExcelTemplates\LEED PostProcess r33.xlsx"

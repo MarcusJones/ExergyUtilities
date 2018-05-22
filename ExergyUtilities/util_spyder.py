5beb3e4def2b7e846b948e62f2b4ae4bbdf30fa6
MarcusJones <jones.0bj3@gmail.com>
2018-05-22 20:54:35 +0200

    NMC

--- ExergyUtilities/util_spyder.py
+++ ExergyUtilities/util_spyder.py
@@ -4,10 +4,21 @@ import datetime
 class SpyderLog:
     def __init__(self,this_format):
         self.this_format = this_format
+        self.last_time = datetime.datetime.now()
+    
     def debug(self,msg):
-        print(self.time, msg)
+        print(self.time, self.elapsed, "DBG", msg)
+        self.last_time = datetime.datetime.now()
+    
     def info(self,msg):
-        print(self.time, msg)
+        print(self.time,  self.elapsed, "INF", msg)
+        self.last_time = datetime.datetime.now()
+    
+    @property
+    def elapsed(self):
+        tdelta = datetime.datetime.now() - self.last_time
+        return "{:>3.2f}".format(tdelta.seconds / 60)
+    
     @property
     def time(self):
         return datetime.datetime.now().strftime('%H:%M:%S')
